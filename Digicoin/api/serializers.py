91d6b0761ecacdc7f215e459f28143b2b7a5a467
kinokano <kin2014@hotmail.com>
2025-03-20 20:12:30 -0400

    Update - Models

--- Digicoin/api/serializers.py
+++ Digicoin/api/serializers.py
@@ -1,3 +1,35 @@
 from rest_framework import serializers
 
 from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class CampanhaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Campanha
+        fields = '__all__'
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+class DesafioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Desafio
+        fields = '__all__'
+
+class CompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compra
+        fields = '__all__'
+
+class ItensCompraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = '__all__'
+
+        
