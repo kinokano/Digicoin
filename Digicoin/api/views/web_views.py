5ac46d632aaf13162e9bb89a8c5c152afaf2a1fb
lrocha97 <lucas.rocha2902@gmail.com>
2025-03-26 21:30:03 -0400

    atualizando pagina do estoque com js

--- Digicoin/api/views/web_views.py
+++ Digicoin/api/views/web_views.py
@@ -34,4 +34,32 @@ def cadastrarDesafio(request):
 def ranking(request):
     return render(request, 'UserHtml/ranking.html')
 def listaEstoque(request):
-    return render(request, 'AdmHtml/listaEstoque.html')
+    estoque = [ 
+
+    {
+        'id': 1,
+        'nome': "Produto 1 ",
+        'qtd': 2,
+        'valor': "1900",
+    },
+    {
+        'id': 2,
+        'nome': "Produto 2 ",
+        'qtd': 2,
+        'valor': "2900",
+    },
+    {
+        'id': 3,
+        'nome': "Produto 3 ",
+        'qtd': 2,
+        'valor': "3900",
+    },
+    {
+        'id': 4,
+        'nome': "Produto 4 ",
+        'qtd': 2,
+        'valor': "4900",
+    }
+  
+    ]
+    return render(request, 'AdmHtml/listaEstoque.html', {'estoque': estoque})
