6ae6f377477d8db630fed5d009fc53eadfb09db9
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2025-04-15 20:18:21 -0400

    Update listaProdutos.js

--- Digicoin/api/static/js/UserJs/listaProdutos.js
+++ Digicoin/api/static/js/UserJs/listaProdutos.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const dialog = document.getElementById("modal");
-    const flipCard = document.querySelector(".flip");
-    const fecharBtns = document.querySelectorAll("#fechar, #fechar2");
-    const imgButtons = document.querySelectorAll(".imgD button"); 
-    const btnVirarTras = document.getElementById("refresh");
-    const btnVirarFrente = document.getElementById("refresh2");
-    const adquirirBtns = document.querySelectorAll(".Adquirir");
+    const produtos = document.getElementsByClassName("imgD");
+
+    for (let i = 0; i < produtos.length; i++) {
+        produtos[i].children[0].addEventListener("click", function () {
+            const idProduto = produtos[i].children[0].dataset.valor;
+            const dialog = document.getElementById(`modal-${idProduto}`);
+            const flipCard = document.getElementById(`flip-${idProduto}`);
+            const refresh = flipCard.querySelector(".frente .refresh");
+            const refresh2 = flipCard.querySelector(".tras .refresh");
+            const fecharBtns = dialog.querySelectorAll(".fechar");
 
             dialog.showModal();
 
@@ -39,64 +42,5 @@ document.addEventListener("DOMContentLoaded", function () {
                 flipCard.dataset.listenersAdded = "true";
             }
         });
-
-    fecharBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            dialog.close();
-            flipCard.classList.remove("virado", "virado2");
-        });
-    });
-
-    btnVirarTras.addEventListener("click", () => {
-        flipCard.classList.remove("virado2");
-        flipCard.classList.toggle("virado");
-    });
-
-    btnVirarFrente.addEventListener("click", () => {
-        flipCard.classList.remove("virado");
-        flipCard.classList.toggle("virado2");
-    });
-
-    dialog.addEventListener("click", (event) => {
-        const container = document.querySelector(".container");
-        if (!container.contains(event.target)) {
-            dialog.close();
-            flipCard.classList.remove("virado", "virado2"); // Reseta para a primeira página
-        }
-    });
-
-    // Função para adicionar produto ao localStorage
-    function adicionarProdutoAoLocalStorage(produto) {
-        let listaProdutos = JSON.parse(localStorage.getItem('listaProdutos')) || { listaGrid: [] };
-        listaProdutos.listaGrid.push(produto);
-        localStorage.setItem('listaProdutos', JSON.stringify(listaProdutos));
     }
-
-    // Adiciona evento de clique ao botão "Adquirir"
-    adquirirBtns.forEach((btn) => {
-        const idProduto = btn.getAttribute("data-id");
-        console.log(idProduto);
-        const tipo = document.querySelector(`input[name="tipoProduto[${idProduto}]"]`).value;
-        console.log(tipo);
-        let tipoFisico;
-        if (tipo === "Fisico") {
-            tipoFisico = true;
-        }else {
-            tipoFisico = false;
-        }
-        btn.addEventListener("click", () => {
-            const produto = {
-                id: idProduto, // Gera um ID único baseado no timestamp atual
-                idProduto: idProduto, // Você pode ajustar conforme necessário
-                nomeProduto: document.querySelector(`input[name="nomeProduto[${idProduto}]"]`).value,
-                valorProduto: document.querySelector(`input[name="valorProduto[${idProduto}]"]`).value,
-                qtdProduto: 1, // Quantidade padrão, ajuste conforme necessário
-                fisicoProduto: tipoFisico // Ajuste conforme necessário
-            };
-            console.log(produto);
-            adicionarProdutoAoLocalStorage(produto);
-            dialog.close();
-            flipCard.classList.remove("virado", "virado2");
-        });
 });
-
